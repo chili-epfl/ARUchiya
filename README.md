d2387e190aa818194f772e1b99eee9062b59885a
Sergei Volodin <etoestja@yandex.ru>
2017-11-01 12:28:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### UchiyaMarkers
+
+Random dots markers for AR
+
+Taken and adapted from
+http://limu.ait.kyushu-u.ac.jp/~uchiyama/me/code/UCHIYAMARKERS/index.html
---
+++ mylib/opencvpath.h
@@ -0,0 +1,19 @@
+/*
+	This file is part of UCHIYAMARKERS, a software for random dot markers.
+	Copyright (c) 2011 Hideaki Uchiyama
+
+	You can use, copy, modify and re-distribute this software
+	for non-profit purposes.
+*/
+
+#ifndef OPENCVPATH_H
+#define OPENCVPATH_H
+
+
+#include "opencv2/calib3d/calib3d_c.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+
+#define cvCopyImage cvCopy
+
+#endif
